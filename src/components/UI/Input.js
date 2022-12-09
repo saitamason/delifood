c4e534390efe657e7538ded60d0b6177343fed71
dawidlehai <haidawidle@gmail.com>
2022-12-09 09:37:17 +0100

    Created MealItemForm component and working on reusable input component

---
+++ src/components/UI/Input.js
@@ -0,0 +1,12 @@
+import classes from "./Import.module.css";
+
+const Input = (props) => {
+  return (
+    <div className={classes.input}>
+      <label></label>
+      <input />
+    </div>
+  );
+};
+
+export default Input;
