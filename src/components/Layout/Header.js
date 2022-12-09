fa993f658e814fe0d1e9881214c414fe40114f13
dawidlehai <haidawidle@gmail.com>
2022-12-09 08:45:05 +0100

    Added CSS classes to the Header component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,15 +1,16 @@
 import { Fragment } from "react";
 
 import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
 
 const Header = (props) => {
   return (
     <Fragment>
-      <header>
+      <header className="{classes.header}">
         <h1>Meals</h1>
         <button>Cart</button>
       </header>
-      <div>
+      <div className="{classes['main-image']}">
         <img src={mealsImage} alt="A table full of delicious food!" />
       </div>
     </Fragment>
