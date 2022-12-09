193903ca035e0f59b5fca762d2fafd8ad268cd0c
dawidlehai <haidawidle@gmail.com>
2022-12-09 08:51:45 +0100

    Fixed errors

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -6,11 +6,11 @@ import classes from "./Header.module.css";
 const Header = (props) => {
   return (
     <Fragment>
-      <header className="{classes.header}">
+      <header className={classes.header}>
         <h1>Meals</h1>
         <button>Cart</button>
       </header>
-      <div className="{classes['main-image']}">
+      <div className={classes["main-image"]}>
         <img src={mealsImage} alt="A table full of delicious food!" />
       </div>
     </Fragment>
