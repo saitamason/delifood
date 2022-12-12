5258011698eee016903ea511be853ebd0fd9ab1d
dawidlehai <haidawidle@gmail.com>
2022-12-12 18:26:13 +0100

    Fetching meals data from Firebase API/database

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,10 +1,34 @@
+import { useState, useEffect } from "react";
 import MealItem from "./MealItem/MealItem";
 import Card from "../UI/Card";
-import meals from "../../data/meals";
 import classes from "./AvailableMeals.module.css";
 
 const AvailableMeals = () => {
-  const mealsList = meals.map((meal) => (
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    const getMeals = async () => {
+      const response = await fetch(
+        "https://dawidlehai-delifood-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
+      );
+      const responseData = await response.json();
+
+      const retrievedMeals = Object.entries(responseData).map(([key, meal]) => {
+        return {
+          id: key,
+          name: meal.name,
+          description: meal.description,
+          price: meal.price,
+        };
+      });
+
+      setMeals(retrievedMeals);
+    };
+
+    getMeals();
+  }, []);
+
+  const renderedMeals = meals.map((meal) => (
     <MealItem
       key={meal.id}
       id={meal.id}
@@ -17,7 +41,7 @@ const AvailableMeals = () => {
   return (
     <section className={classes.meals}>
       <Card>
-        <ul>{mealsList}</ul>
+        <ul>{renderedMeals}</ul>
       </Card>
     </section>
   );
